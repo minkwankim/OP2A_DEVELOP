ef350365cce70c356ab9e0c3c2f43ddedbb490bd
M.K.Kim <M.K.Kim@soton.ac.uk>
2016-09-15 13:47:00 +0100

    test

---
+++ Source/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  OP2A
+//
+//  Created by Kim M.K. on 15/09/2016.
+//  Copyright © 2016 Kim M.K. All rights reserved.
+//
+
+#include <iostream>
+#include <mkl.h>
+
+#include "./MATH_MK/matrix.hpp"
+#include "./GRID/element.hpp"
+
+#include "./COMM/error_codes.hpp"
+
+int main(int argc, const char * argv[]) {
+    double res = 10.0;
+    
+    
+    matrix test;
+    ElementBasic<double, double, double> note_test;
+    
+    // insert code here...
+    std::cout << "Hello, World!  " << res << "\n";
+    return 0;
+    
+    
+}
