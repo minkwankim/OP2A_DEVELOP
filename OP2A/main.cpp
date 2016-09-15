f66f053a41da0f0f40979a542bac9dd0541c1f1e
M.K.Kim <M.K.Kim@soton.ac.uk>
2016-09-15 12:42:39 +0100

    Initial Commit

---
+++ OP2A/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OP2A
+//
+//  Created by Kim M.K. on 15/09/2016.
+//  Copyright © 2016 Kim M.K. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
